18a69c9c8955a6235ea45e5649b1346f44bbecf5
Roxanne Garcia <rxrgeneration@gmail.com>
2019-07-31 05:55:18 -0500

    commiting files

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer class="footer text-center">
+    <p>&copy; Company 2017</p>
+  </footer>
+)
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Layout from '../components/Layout'
+
+class Page extends React.Component {
+
+  render() {
+    return (
+      <Layout>
+        <div className="container">
+          <div className="starter text-center">
+            <h1>Bootstrap starter template</h1>
+            <p className="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Page
